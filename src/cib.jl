fd0178a5e7d8f714d925c4cdf3bdfe5b213b702b
Giuseppe Puglisi <giuse.puglisi@gmail.com>
2021-05-03 14:46:56 -0700

    including docstring

--- src/cib.jl
+++ src/cib.jl
@@ -419,6 +419,23 @@ end
 
 
 
+"""
+    paint_with_mask!(result_map, nu_obs, model, sources, fluxes_cen, fluxes_sat,mask_cen, mask_sat)
+
+Paint a source catalog onto a map, recording the fluxes in
+`fluxes_cen` and `fluxes_sat`.
+
+# Arguments:
+- `result_map::Map{T_map, RingOrder}`: Healpix map to paint
+- `nu_obs`: frequency in Hz
+- `model::AbstractCIBModel{T}`: source model parameters
+- `sources`: NamedTuple containing source information from generate_sources
+- `fluxes_cen::AbstractArray`: buffer for writing fluxes of centrals
+- `fluxes_sat::AbstractArray`: buffer for writing fluxes of satellites
+- `mask_cen::AbstractArray`: buffer encoding  the index of the mask which satisfies the condition for  centrals
+- `mask_sat::AbstractArray`: buffer encoding  the index of the mask which satisfies the condition for satellites   
+"""
+
 function paint_with_mask!(result_map::Map{T_map, RingOrder},
     nu_obs, model::AbstractCIBModel{T}, sources,
     fluxes_cen::AbstractArray, fluxes_sat::AbstractArray,
